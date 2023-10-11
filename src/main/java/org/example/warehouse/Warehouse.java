df5a56b69424c88602fdc850175d1dd4043f4ec8
ByHatice <celik.h@outlook.com>
2023-10-11 06:22:35 +0200

    Warehous

--- src/main/java/org/example/warehouse/Warehouse.java
+++ src/main/java/org/example/warehouse/Warehouse.java
@@ -3,22 +3,24 @@ package org.example.warehouse;
 import java.math.BigDecimal;
 import java.util.*;
 import java.util.stream.Collectors;
-
 public class Warehouse {
+    private static Warehouse instance;
     private final String name;
     private final List<ProductRecord> products = new ArrayList<>();
     private final List <ProductRecord> updatedProduct = new ArrayList<>();
     private Warehouse(String name) {
         this.name = name;
     }
-    public boolean isEmpty() {
-        return products.isEmpty();
-    }
     public static Warehouse getInstance() {
-        return new Warehouse("");
+        if (instance == null)
+            instance = new Warehouse("MyStore");
+        return instance;
     }
     public static Warehouse getInstance(String name) {
         return new Warehouse(name);
+    }
+    public boolean isEmpty() {
+        return products.isEmpty();
     }
     public List<ProductRecord> getProducts() {
         return Collections.unmodifiableList(products);
@@ -40,20 +42,20 @@ public class Warehouse {
                 .filter(p -> p.getId().equals(id))
                 .findFirst();
     }
-    public ProductRecord addProduct(UUID uuid, String productName, Category category, BigDecimal price) {
-        if (name == null || name.isEmpty())
+    public ProductRecord addProduct(UUID id, String productName, Category category, BigDecimal price) {
+        if (productName == null || productName.isEmpty())
             throw new IllegalArgumentException("Product name can't be null or empty.");
         if (category == null)
             throw new IllegalArgumentException("Category can't be null.");
         if (price == null)
-            price = BigDecimal.valueOf(0);
-        if(uuid == null)
-            uuid = UUID.randomUUID();
-        if (getProductById(uuid).isPresent())
+            price = BigDecimal.ZERO;
+        if (id == null)
+            id = UUID.randomUUID();
+        if (getProductById(id).isPresent())
             throw new IllegalArgumentException("Product with that id already exists, use updateProduct for updates.");
-        ProductRecord productRecord = new ProductRecord(uuid, productName, category, price);
-        products.add(productRecord);
-        return productRecord;
+        ProductRecord product = new ProductRecord(id, productName, category, price);
+        products.add(product);
+        return product;
     }
     public void updateProductPrice(UUID id, BigDecimal newPrice) {
         Optional<ProductRecord> productToChange = getProductById(id);
